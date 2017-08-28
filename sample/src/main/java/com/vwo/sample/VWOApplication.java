6e4ce5e860fbb399ee64c291ba32834b10b379cc
Amandeep Anguralla <amandeep.anguralla@wingify.com>
2017-08-28 14:07:46 +0530

    Updates sample app

--- sample/src/main/java/com/vwo/sample/VWOApplication.java
+++ sample/src/main/java/com/vwo/sample/VWOApplication.java
@@ -2,36 +2,11 @@ package com.vwo.sample;
 
 import android.app.Application;
 
-import com.google.android.gms.analytics.GoogleAnalytics;
-import com.google.android.gms.analytics.Tracker;
-
-/**
- * This is a subclass of {@link Application} used to provide shared objects for this app, such as
- * the {@link Tracker}.
- */
 public class VWOApplication extends Application {
-
-    private static GoogleAnalytics sAnalytics;
-    private static Tracker sTracker;
 
     @Override
     public void onCreate() {
         super.onCreate();
-
-        sAnalytics = GoogleAnalytics.getInstance(this);
-    }
-
-    /**
-     * Gets the default {@link Tracker} for this {@link Application}.
-     *
-     * @return tracker
-     */
-    synchronized public Tracker getDefaultTracker() {
-        if (sTracker == null) {
-            sTracker = sAnalytics.newTracker(R.xml.global_tracker);
-        }
-
-        return sTracker;
     }
 
 }
