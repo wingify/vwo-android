00610b64d3ad355ddf872ae2a978fcab6d074647
Amandeep Anguralla <amandeep.anguralla@wingify.com>
2017-12-19 16:39:55 +0530

    Predefined Segmentation test cases

--- library/src/test/java/com/vwo/mobile/segmentation/PredefinedSegmentationTest.java
+++ library/src/test/java/com/vwo/mobile/segmentation/PredefinedSegmentationTest.java
@@ -4,16 +4,24 @@ import android.os.Build;
 
 import com.vwo.mobile.BuildConfig;
 import com.vwo.mobile.VWO;
+import com.vwo.mobile.data.VWOPersistData;
 import com.vwo.mobile.mock.ShadowConfiguration;
 import com.vwo.mobile.mock.VWOMock;
 import com.vwo.mobile.mock.VWOPersistDataMock;
+import com.vwo.mobile.utils.VWOUtils;
 
 import junit.framework.Assert;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.rule.PowerMockRule;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
@@ -22,76 +30,99 @@ import org.robolectric.annotation.Config;
  */
 
 @RunWith(RobolectricTestRunner.class)
-@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.JELLY_BEAN, shadows = {ShadowConfiguration.class,
-        VWOPersistDataMock.class }, manifest = "AndroidManifest.xml")
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.JELLY_BEAN, shadows = {
+        VWOPersistDataMock.class}, manifest = "AndroidManifest.xml")
+@PowerMockIgnore({"org.mockito.*", "org.robolectric.*", "android.*", "org.json.*"})
+@PrepareForTest(VWOPersistData.class)
 public class PredefinedSegmentationTest {
+
+    @Rule
+    public PowerMockRule rule = new PowerMockRule();
+
     /**
      * Unit tests for predefined segments
      */
     @Test
-    public void predefinedSegmentationTest() {
+    public void phoneUserTest() throws JSONException {
+        VWO vwo = new VWOMock().getVWOMockObject();
 
-        try {
-            VWO vwo = new VWOMock().getVWOMockObject();
+        String phoneUser = "{\n" +
+                "\"segment_code\": {\n" +
+                "\"device\": \"Phone\"\n" +
+                "},\n" +
+                "\"id\": \"97\",\n" +
+                "\"type\": \"predefined\",\n" +
+                "\"platform\": \"mobile-app\",\n" +
+                "\"name\": \"Phone Users\",\n" +
+                "\"description\": \"Segment for Phone users only\"\n" +
+                "}";
 
-            String phoneUser = "{\n" +
-                    "\"segment_code\": {\n" +
-                    "\"device\": \"Phone\"\n" +
-                    "},\n" +
-                    "\"id\": \"87\",\n" +
-                    "\"type\": \"predefined\",\n" +
-                    "\"platform\": \"mobile-app\",\n" +
-                    "\"name\": \"Phone Users\",\n" +
-                    "\"description\": \"Segment for Phone users only\"\n" +
-                    "}";
+        PredefinedSegment segmentPhoneUser = new PredefinedSegment(new JSONObject(phoneUser));
+        Assert.assertTrue(segmentPhoneUser.evaluate(vwo));
+    }
 
-            String tabletUser = "{\n" +
-                    "\"segment_code\": {\n" +
-                    "\"device\": \"Tablet\"\n" +
-                    "},\n" +
-                    "\"id\": \"88\",\n" +
-                    "\"type\": \"predefined\",\n" +
-                    "\"platform\": \"mobile-app\",\n" +
-                    "\"name\": \"Tablet Users\",\n" +
-                    "\"description\": \"Segment for Tablet users only\"\n" +
-                    "}";
+    @Test
+    @Config(shadows = {ShadowConfiguration.class,
+            VWOPersistDataMock.class})
+    public void tabletUserTest() throws JSONException {
+        VWO vwo = new VWOMock().getVWOMockObject();
+        String tabletUser = "{\n" +
+                "\"segment_code\": {\n" +
+                "\"device\": \"Tablet\"\n" +
+                "},\n" +
+                "\"id\": \"88\",\n" +
+                "\"type\": \"predefined\",\n" +
+                "\"platform\": \"mobile-app\",\n" +
+                "\"name\": \"Tablet Users\",\n" +
+                "\"description\": \"Segment for Tablet users only\"\n" +
+                "}";
 
-            String newUser = "{\n" +
-                    "\"segment_code\": {\n" +
-                    "\"returning_visitor\": false\n" +
-                    "},\n" +
-                    "\"id\": \"89\",\n" +
-                    "\"type\": \"predefined\",\n" +
-                    "\"platform\": \"mobile-app\",\n" +
-                    "\"name\": \"New Users\",\n" +
-                    "\"description\": \"Segment for new users only\"\n" +
-                    "}";
+        PredefinedSegment segmentTabletUser = new PredefinedSegment(new JSONObject(tabletUser));
+        Assert.assertTrue(segmentTabletUser.evaluate(vwo));
+    }
 
-            String returningUser = "{\n" +
-                    "\"segment_code\": {\n" +
-                    "\"returning_visitor\": true\n" +
-                    "},\n" +
-                    "\"id\": \"90\",\n" +
-                    "\"type\": \"predefined\",\n" +
-                    "\"platform\": \"mobile-app\",\n" +
-                    "\"name\": \"Returning Users\",\n" +
-                    "\"description\": \"Segment for returning users only\"\n" +
-                    "}";
+    @Test
+    public void newUserTest() throws JSONException {
+        VWO vwo = new VWOMock().getVWOMockObject();
 
-            PredefinedSegment predefinedSegmentIsNotPhoneUser = new PredefinedSegment(new JSONObject(phoneUser));
-            Assert.assertEquals(predefinedSegmentIsNotPhoneUser.evaluate(vwo), false);
+        PowerMockito.mockStatic(VWOPersistData.class);
+        PowerMockito.when(VWOPersistData.isReturningUser(ArgumentMatchers.any(VWO.class))).thenReturn(false);
 
-            PredefinedSegment predefinedSegmentIsTabletUser = new PredefinedSegment(new JSONObject(tabletUser));
-            Assert.assertEquals(predefinedSegmentIsTabletUser.evaluate(vwo), true);
+        String newUser = "{\n" +
+                "\"segment_code\": {\n" +
+                "\"returning_visitor\": false\n" +
+                "},\n" +
+                "\"id\": \"103\",\n" +
+                "\"type\": \"predefined\",\n" +
+                "\"platform\": \"mobile-app\",\n" +
+                "\"name\": \"New Users\",\n" +
+                "\"description\": \"Segment for new users only\"\n" +
+                "}";
 
-            PredefinedSegment predefinedSegmentIsNewUser = new PredefinedSegment(new JSONObject(newUser));
-            Assert.assertEquals(predefinedSegmentIsNewUser.evaluate(vwo), false);
+        PredefinedSegment segmentNewUser = new PredefinedSegment(new JSONObject(newUser));
+        Assert.assertTrue(segmentNewUser.evaluate(vwo));
+    }
 
-            PredefinedSegment predefinedSegmentIsReturningUser = new PredefinedSegment(new JSONObject(returningUser));
-            Assert.assertEquals(predefinedSegmentIsReturningUser.evaluate(vwo), true);
+    @Test
+    public void returningUserTest() throws JSONException {
+        VWO vwo = new VWOMock().getVWOMockObject();
 
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        PowerMockito.mockStatic(VWOPersistData.class);
+        PowerMockito.when(VWOPersistData.isReturningUser(ArgumentMatchers.any(VWO.class))).thenReturn(true);
+
+        String returningUser = "{\n" +
+                "\"segment_code\": {\n" +
+                "\"returning_visitor\": true\n" +
+                "},\n" +
+                "\"id\": \"90\",\n" +
+                "\"type\": \"predefined\",\n" +
+                "\"platform\": \"mobile-app\",\n" +
+                "\"name\": \"Returning Users\",\n" +
+                "\"description\": \"Segment for returning users only\"\n" +
+                "}";
+
+
+        PredefinedSegment segmentReturningUser = new PredefinedSegment(new JSONObject(returningUser));
+        Assert.assertEquals(segmentReturningUser.evaluate(vwo), true);
     }
 }
