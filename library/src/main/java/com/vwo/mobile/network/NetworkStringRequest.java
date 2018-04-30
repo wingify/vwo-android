dc6719600462583017f67814e90085a59be4e6bb
Amandeep Anguralla <amandeep.anguralla@wingify.com>
2018-04-30 09:26:31 +0530

    Bug fix

--- library/src/main/java/com/vwo/mobile/network/NetworkStringRequest.java
+++ library/src/main/java/com/vwo/mobile/network/NetworkStringRequest.java
@@ -155,9 +155,10 @@ public class NetworkStringRequest extends NetworkRequest<String> {
         if (body != null) {
             if (gzipEnabled) {
                 VWOLog.i(VWOLog.UPLOAD_LOGS, body, true);
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                ByteArrayOutputStream baos = null;
                 GZIPOutputStream gzos = null;
                 try {
+                    baos = new ByteArrayOutputStream();
                     gzos = new GZIPOutputStream(baos);
                     gzos.write(body.getBytes(DEFAULT_CONTENT_ENCODING));
                     gzos.flush();
@@ -165,6 +166,11 @@ public class NetworkStringRequest extends NetworkRequest<String> {
                     VWOLog.e(VWOLog.UPLOAD_LOGS, exception, false, true);
                     return super.getBody();
                 } finally {
+                    if(baos != null) {
+                        try {
+                            baos.close();
+                        } catch (IOException ignore) { }
+                    }
                     if (gzos != null) try {
                         gzos.close();
                     } catch (IOException ignore) { }
