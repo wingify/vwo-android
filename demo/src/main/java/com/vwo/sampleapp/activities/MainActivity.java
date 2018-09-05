1a1472017bc47b9bda8b6c7b4b6cc06f1ae88ea4
Amandeep Anguralla <amandeep400@gmail.com>
2018-09-05 10:42:54 +0530

    Minor change

--- demo/src/main/java/com/vwo/sampleapp/activities/MainActivity.java
+++ demo/src/main/java/com/vwo/sampleapp/activities/MainActivity.java
@@ -262,7 +262,7 @@ public class MainActivity extends BaseActivity
             VWOConfig.Builder vwoConfigBuilder = new VWOConfig.Builder();
 //            vwoConfigBuilder.disablePreview();
             vwoConfigBuilder.setOptOut(false);
-            vwoConfigBuilder.userID("amandeep.anguralla@wingify.com");
+            vwoConfigBuilder.userID("Amandeep.anguralla@wingify.com");
             if (keys == null) {
                 keys = new HashMap<>();
             }
